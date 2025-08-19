f9c90ca44921e3173c7690ccb06d70e323398c7d
paul.peytevin@student-cs.fr <peytevin_pau@centralesupelec.fr>
2025-08-19 11:29:41 +0200

    on a fait tourner sur la dgx. Maintenant nous avons un autre problème avec l'entrainement qui semble parfois être coincé de manière aléatoire

--- cant_stop/train_rl_agent.py
+++ cant_stop/train_rl_agent.py
@@ -14,7 +14,7 @@ import time
 
 model_dir = f"models/PPO-{int(time.time())}"
 log_dir = f"logs/PPO-{int(time.time())}"
-total_timesteps = 200000
+total_timesteps = 1000000
 
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     env.reset()
     env = ActionMasker(env, mask_fn)
     model = MaskablePPO("MlpPolicy", env=env, verbose=1, tensorboard_log=log_dir)
-    for i in range(1, 12):
+    for i in range(1, 20):
         model.learn(total_timesteps=total_timesteps, reset_num_timesteps=False, tb_log_name="ppo_cant_stop")
         model.save(os.path.join(model_dir, f"ppo_cant_stop_{total_timesteps * i}"))
         print("Modèle PPO entraîné et sauvegardé.")
