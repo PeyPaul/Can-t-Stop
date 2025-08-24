34e0e4067e5a37b1f56a34dabb95ede8d84ea3b9
Paul Peytevin <ppeytevin@gmail.com>
2025-08-24 13:01:23 +0200

    j'ai ajouter une nouvelle métrique : max turn

--- cant_stop/players/rl_agent.py
+++ cant_stop/players/rl_agent.py
@@ -11,8 +11,8 @@ from environments.gym_env_v2 import CantStopGymEnv
 import sys
 import os
 
-model_dir = "models/PPO-1755694166"
-model_path = os.path.join(model_dir, "ppo_cant_stop_10000000.zip")
+model_dir = "models/PPO-1755942961"
+model_path = os.path.join(model_dir, "ppo_cant_stop_19000000.zip")
         
 class RLAgent:
     def __init__(self, name): # ça a l'air bon
--- cant_stop/train_rl_agent.py
+++ cant_stop/train_rl_agent.py
@@ -2,6 +2,7 @@
 
 from stable_baselines3.common.callbacks import BaseCallback
 import time
+import numpy as np
 
 class WatchdogCallback(BaseCallback):
     def __init__(self, max_silence_sec=1200, verbose=1):
@@ -21,6 +22,29 @@ class WatchdogCallback(BaseCallback):
                 return False  # stoppe l'entraînement proprement
         if self.n_calls % 10000 == 0:
             print(f"[HB] {self.num_timesteps} steps...")
+        return True
+    
+class EpisodeTurnMaxCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super(EpisodeTurnMaxCallback, self).__init__(verbose)
+        self.episode_turns = []
+
+    def _on_step(self) -> bool:
+        infos = self.locals.get("infos", [])
+        dones = self.locals.get("dones", [])
+
+        for info, done in zip(infos, dones):
+            if "turn" in info:
+                self.episode_turns.append(info["turn"])
+
+            # Si un épisode est terminé → calcul et log
+            if done and len(self.episode_turns) > 0:
+                max_turn = np.max(self.episode_turns)
+                self.logger.record("custom/max_turn_per_episode", max_turn)
+                if self.verbose > 0:
+                    print(f"[EpisodeTurnMaxCallback] Max turn for episode: {max_turn}")
+                self.episode_turns = []  # reset pour le prochain épisode
+
         return True
     
 
@@ -60,7 +84,7 @@ if __name__ == "__main__":
     env.reset()
     env = ActionMasker(env, mask_fn)
     model = MaskablePPO("MlpPolicy", env=env, verbose=1, tensorboard_log=log_dir)
-    callback = WatchdogCallback(max_silence_sec=1200)
+    callback = [WatchdogCallback(max_silence_sec=1200), EpisodeTurnMaxCallback(verbose=1)]
     for i in range(1, 20):
         model.learn(total_timesteps=total_timesteps, reset_num_timesteps=False, tb_log_name="ppo_cant_stop", callback=callback, progress_bar=True)
         model.save(os.path.join(model_dir, f"ppo_cant_stop_{total_timesteps * i}"))
